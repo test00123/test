eac8c7a09ff81f71de0462e0c428f1b02e790dab
Mukeshdhaka12 <121882652+Mukeshdhaka12@users.noreply.github.com>
2023-01-17 20:56:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -379,7 +379,7 @@ def humanbytes(size):
 
 #shortzy 
 
-shortz = shortzy.Shortzy(SHORTENER_API, "OmegaLinks.in")
+shortz = shortzy.Shortzy(SHORTENER_API, "omegalinks.in")
 async def get_shortlink(link):
     if SHORTENER_API:
         if LONG_DROPLINK_URL =="True" or LONG_DROPLINK_URL is True:

066179a50bc55070f9d046fdfd36ca5f59f999c7
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-03-06 14:07:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -731,8 +731,8 @@ async def advantage_spell_chok(msg):
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
     query = query.strip() + " movie"
-    g_s = await get_poster(query)
-    g_s = await get_poster(msg.text)
+    g_s = await search_gagala(query)
+    g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
         k = await msg.reply("I couldn't find any movie in that name.")
